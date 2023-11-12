be48fbd6ab70540ad02f638338e56a14686354f3
Enrique <enriquearce00@gmail.com>
2023-11-12 10:33:40 -0600

    Starting...

---
+++ Products/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Products
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
