de2635ca5744864929f5a2cbdc6e81aeeb9a3520
Enrique <enriquearce00@gmail.com>
2023-11-12 11:20:36 -0600

    Product Data Template Selector

---
+++ Products/Selectors/ProductDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using Products.Models;
+
+namespace Products.Selectors
+{
+    public class ProductDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var model = item as Product;
+
+            Application
+                .Current
+                .Resources
+                .TryGetValue("SimpleProductDataTemplate", out var simpleProductDataTemplate);
+
+            if (model != null)
+            {
+                if(model.HasOffer)
+                {
+                    return new DataTemplate();
+                }
+            }
+
+            return simpleProductDataTemplate as DataTemplate;
+        }
+    }
+}
